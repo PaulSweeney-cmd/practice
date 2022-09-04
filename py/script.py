d5c30bfc4083dedcda61996463676a21ce51b34c
PaulSweeney-cmd <paulsweeney474@gmail.com>
2022-09-04 21:14:33 +0000

    Updated script.py file

--- py/script.py
+++ py/script.py
@@ -6,58 +6,58 @@ import math
 
 
 # ! Python Syntax - PRINT STATEMENTS
-# ? allows the programme to have a conversation with the user
-# ? python-2 syntax print statements have no parentheses, in python-3 they do
+# allows the programme to have a conversation with the user
+# python-2 syntax print statements have no parentheses, in python-3 they do
 
 print('Hello and welcome back')
 
 # ! Python Syntax - STRINGS
-# ? strings contain letters, numbers and symbols in a specific order
-# ? strings are basically text
-# ? strings can be encased in either single or double quotes
+# strings contain letters, numbers and symbols in a specific order
+# strings are basically text
+# strings can be encased in either single or double quotes
 
-# ? you can also use an escaper if you needed to use another single quote
+# you can also use an escaper if you needed to use another single quote
 print('I am a string written with an escaper in paul\'s example')
 
-# * Combining or 'concatenating' strings
+# Combining or 'concatenating' strings
 print("This string has been " + "concatenated")
 
 # * EOL error (End Of Line) - EOL while scanning a string literal
-# ? Python interpreter expected a particular character or set of characters
+# Python interpreter expected a particular character or set of characters
 
 # * NameError
-# ? Python doesnt recognise the command due to it not being defined anywhere
+# Python doesnt recognise the command due to it not being defined anywhere
 
 # ! Python Syntax - VARIABLES
-# ? Defining things that are subject to change
-# ? they are case sensitive
-# ? They always include an underscore
+# Defining things that are subject to change
+# they are case sensitive
+# They always include an underscore
 
 # ! Python Syntax - UPDATING VARIABLES
 
 DAYS_WORKED = 6
 PARCELS_DELIVERED = 150
 
-# * updating the variable to calculate how many parcels in total were delivered
+# updating the variable to calculate how many parcels in total were delivered
 DAYS_WORKED *= PARCELS_DELIVERED
 print(DAYS_WORKED)
 
 # ! Python Syntax - ARITHMATIC
-# ? Operators + / * -
-# ? MODULO OPERATOR (%) = returns a remainder after the division
-# ? **(to the power of) = 3 ** 4 is 3 multiplied by itself 4 times
+# Operators + / * -
+# MODULO OPERATOR (%) = returns a remainder after the division
+# (to the power of) = 3 ** 4 is 3 multiplied by itself 4 times
 
 # ! Python Syntax - NUMBERS
-# ? Variables can hold one of two numbers, FLOAT AND INTEGER
-# ? Floats can be declared by adding a decimal to the end of an integer
-# ? You can define a float using scientific notation by adding e2:
+# Variables can hold one of two numbers, FLOAT AND INTEGER
+# Floats can be declared by adding a decimal to the end of an integer
+# You can define a float using scientific notation by adding e2:
 
 POWER_OF_TEN_FLOAT = 1.5e2  # * 1.5 to the power of 10 (e2) = 150
 print(POWER_OF_TEN_FLOAT)
 
 # ! Python Syntax - TWO TYPES OF DIVISION
-# ? you can divide numbers using conventional /
-# ? you can add a decimal if you were dividing uneven numbers to get a float:
+# you can divide numbers using conventional /
+# you can add a decimal if you were dividing uneven numbers to get a float:
 
 DIVIDE_FLOAT_ONE = 7/2
 print(DIVIDE_FLOAT_ONE)
@@ -68,30 +68,30 @@ QUOTIENT = float(10/3)
 print(QUOTIENT)
 
 # ! Python Syntax - DATA CONVERSION
-# ? Methods to convert a data type are as follows
+# Methods to convert a data type are as follows
 
-# ? EXAMPLE - converting a string to use the value in an arithmatic operation
+# EXAMPLE - converting a string to use the value in an arithmatic operation
 FLOAT_DATA_TYPE = "2.1"
 UPDATED_DATA_TYPE = float(FLOAT_DATA_TYPE)
 print(FLOAT_DATA_TYPE)
 
-# ? EXAMPLE - converting a string to use the value in an arithmatic operation
+# EXAMPLE - converting a string to use the value in an arithmatic operation
 INTEGER_DATA_TYPE = "100"
 UPDATED_INTEGER_DATA_TYPE = int(INTEGER_DATA_TYPE)
 print(INTEGER_DATA_TYPE)
 
-# ? EXAMPLE - conveerting a floating point number to an integer
-# ? this will round the integer down to the number closer towards 0
+# EXAMPLE - conveerting a floating point number to an integer
+# this will round the integer down to the number closer towards 0
 FLOAT_TO_INTEGER = 22.5
 UPDATED_FLOAT_TO_INTEGER = int(FLOAT_TO_INTEGER)
 print(UPDATED_FLOAT_TO_INTEGER)  # prints 22
 
-# ? EXAMPLE - converting an integer to a string to combine with another string
+# EXAMPLE - converting an integer to a string to combine with another string
 STRING_DATA_TYPE = 100
 UPDATED_STRING_DATA_TYPE = str(STRING_DATA_TYPE)
 print(STRING_DATA_TYPE)
 
-# ? If you use int() on a float string value, it will raise a ValueError
+# If you use int() on a float string value, it will raise a ValueError
 
 FLOAT_1 = 0.25
 FLOAT_2 = 40.0
@@ -115,20 +115,20 @@ print(NOW.month)
 print(NOW.year)
 
 # placing the data in to placeholders in a string
-# ? 02d pads the day and month to two decimal places starting at 0
-# ? 04d pads the year to 4 plcaes
-# ? These decimal places can vary depending on what data you want to display
+# 02d pads the day and month to two decimal places starting at 0
+# 04d pads the year to 4 plcaes
+# These decimal places can vary depending on what data you want to display
 
 JLS_EXTRACT_VAR = "%02d/%02d/%04d"
 print(JLS_EXTRACT_VAR % (NOW.day, NOW.month, NOW.year))
 
 # ! Conditionals and Control Flow - Truthy or Falsy
-# ? Conditionals and control flow is to access the outcome of a condition
+# Conditionals and control flow is to access the outcome of a condition
 
 # ! Conditionals and Control Flow - Boolean operators
-# ? NOT = gives the opposite condition of the statement - gets evaluated first
-# ? AND = checks if both statements are true - gets evaluated next
-# ? OR = checks if at least one of the statements is true - gets evaluated last
+# NOT = gives the opposite condition of the statement - gets evaluated first
+# AND = checks if both statements are true - gets evaluated next
+# OR = checks if at least one of the statements is true - gets evaluated last
 
 
 # * not False  # returns True
@@ -146,7 +146,7 @@ else:
     print('Bad job!')
 
 
-# ? using a conditional stement in a function
+# using a conditional stement in a function
 
 
 def stating_true():
@@ -182,7 +182,7 @@ def func_with_args(result):
 func_with_args(10)
 
 # ! PygLatin - input('a string')
-# ? input accepts a string, prints it and then waits for the user to type
+# input accepts a string, prints it and then waits for the user to type
 
 
 def users_input():
@@ -197,7 +197,7 @@ def users_input():
 # users_input()
 
 # ! PygLatin - .isalpha()
-# ? .isalpha checks whether a given string contains alphabetic characters
+# .isalpha checks whether a given string contains alphabetic characters
 
 
 def check_alpha_character():
@@ -225,14 +225,14 @@ else:
 
 
 # ! PygLatin - translating to Pig Latin
-# ? Rules for translation - move the first letter of the word to the end
-# ? Rules for translation - you then add the ay suffix to the end
+# Rules for translation - move the first letter of the word to the end
+# Rules for translation - you then add the ay suffix to the end
 
 
 # ! Functions - Params & Args
-# ? Can accept parameters and argumants
-# ? Parameters are placeholders for arguments
-# ? Inputs (or 'values') in to the functions are arguments
+# Can accept parameters and argumants
+# Parameters are placeholders for arguments
+# Inputs (or 'values') in to the functions are arguments
 
 JLS_EXTRACT_VAR = "%d to the power of %d is %d."
 
@@ -290,23 +290,23 @@ def by_three(number):
 print(by_three(30))  # prints 27000 / 30 * 30 * 30
 
 # ! Functions - MODULE IMPORTS (import math)
-# ? A module (a file) contains definitions that include variables and functions
-# ? Using the sqrt (square root) function from the math module
+# A module (a file) contains definitions that include variables and functions
+# Using the sqrt (square root) function from the math module
 
 print(math.sqrt(25))
 
 # ! Functions - FUNCTION IMPORTS (from math import sqrt)
-# ? pulling a singe function from a module is called a function import
+# pulling a single function from a module is called a function import
 
 # ! Functions - UNIVERSAL IMPORT (from math import *)
 
-# ? A universal import is importing everything from a module
-# ? this can be dangerous as everything gets imported and takes up space
-# ? You may not need everything so just be specific on what you want to use
-# ? This may lead to two functions with the same name and cause conflict
+# a universal import is importing everything from a module
+# this can be dangerous as everything gets imported and takes up space
+# You may not need everything so just be specific on what you want to use
+# This may lead to two functions with the same name and cause conflict
 
 # ! Functions - BUILT IN FUNCTIONS
-# ? Built in functions don't need importing
+# Built in functions don't need importing
 
 # - max() = returns the largest number
 MAXIMUM = max(550, 456, 13456, 77)
@@ -396,9 +396,9 @@ def trip_cost(city, days, spending_money):
 print(trip_cost('Los Angeles', 5, 600))
 
 # ! Lists and Dictionaries -
-# ? A data type used to store different pieces of information
-# ? Lists have items - empty lists have ()
-# ? Dictionaries have key : value pairs - empty dictionaries have {}
+# A data type used to store different pieces of information
+# Lists have items - empty lists have ()
+# Dictionaries have key : value pairs - empty dictionaries have {}
 
 # ! Lists and Dictionaries - indexing a single item in a list
 LIST_ONE = 'Hellothisisasinglestring'
@@ -431,7 +431,7 @@ for number in my_list:
     print(2 * number)
 
 # ! Lists and Dictionaries - sorting through a list
-# ? using te sort() method we can sort each item in alphabetical order
+# using te sort() method we can sort each item in alphabetical order
 
 start_list = [5, 3, 1, 2, 4]
 square_list = []
@@ -447,13 +447,13 @@ print(square_list)
 # [1, 4, 9, 16, 25]
 
 # ! Lists and Dictionaries - key : value pairs
-# ? here you can access the value by indexing the key
+# here you can access the value by indexing the key
 ANIMALS = {'Dog': 300, 'Cat': 301, 'Bird': 302, 'Turtle': 303}
 print(ANIMALS['Cat'])
 
 # ! Lists and Dictionaries - adding a new dictionary entry
-# ? an important thing to remember is dictionaries are mutable just like lists
-# ? this means they can be changed after they're created
+# an important thing to remember is dictionaries are mutable just like lists
+# this means they can be changed after they're created
 
 # creating an empty dictionary
 DESTINATIONS = {}
